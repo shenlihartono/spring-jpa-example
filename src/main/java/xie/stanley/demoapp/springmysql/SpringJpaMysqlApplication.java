5491e6a2638f58a19f4bb7ddc52dd3e5e59f0297
Stanley <shenlihartono@gmail.com>
2020-06-28 13:17:51 +0700

    setup project with actuator

---
+++ src/main/java/xie/stanley/demoapp/springmysql/SpringJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package xie.stanley.demoapp.springmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xie/stanley/demoapp/springmysql/SpringJpaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package xie.stanley.demoapp.springmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
